b25c2b9e8ac6a74f756622307159d2f60cbb5ca5
Dilan Patel <dilan.patel@solirius.com>
2025-10-31 10:06:33 +0000

    allow any source address

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     protocol                   = "Tcp"    
     source_port_range          = "*"
     destination_port_range     = "22" 
-    source_address_prefix      = var.my_public_ip
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
   security_rule {

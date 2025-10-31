76e1f636122a4905d059b987b513ff23a4310c7a
Dilan Patel <dilan.patel@solirius.com>
2025-10-31 13:21:55 +0000

    firewall rule to allow GitHub actions run

--- main.tf
+++ main.tf
@@ -60,6 +60,7 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+  
 }
 
 # Create virtual NIC for VM
